10c7ac4ea616a36cc001ece473f9b6cfcc0fccda
Abhishek9267 <200424380@student.georgianc.on.ca>
2020-01-16 23:21:34 -0500

    Completing Lab2

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+
+            string[] product = new string[3];
+            product[0] = "Laptop";
+            product[1] = "Tablet";
+            product[2] = "Desktop";
+            ViewData["product"] = product;
+            return View();
+        }
+        public string Details(string product)
+        {
+
+
+
+            string message = "You have selected the product" + product;
+            return message;
+        }
+
+    }
+}
